25df52c5631fda1fd85a3a3e94406dd1f597bb8b
nicholas a. evans <nick@ekenosen.net>
2010-09-27 14:41:21 -0400

    fill out more monit/startup script example

--- examples/start_resque_pool_with_pid.sh
+++ examples/start_resque_pool_with_pid.sh
@@ -9,8 +9,8 @@
 # they are set to exactly what I expect, no matter how the app is run.
 # Setting these here may not be necessary for your environment.
 export HOME=/home/appuser
-# Make sure we are using the correct rake with the correct ruby
-RAKE='/opt/ruby-enterprise-1.8.6-20090610/bin/rake'
+# Make sure we are using the correct bundler with the correct ruby
+BUNDLE='/opt/ruby-enterprise-1.8.6-20090610/bin/bundle'
 export PATH=/opt/ruby-enterprise-1.8.6-20090610/bin:/usr/local/bin:/usr/bin:/bin
 
 RAILS_ROOT=$1
@@ -27,5 +27,5 @@ export RAILS_ENV=production
 #export VVERBOSE=true
 
 cd $RAILS_ROOT
-$RAKE --trace resque:pool >> $STDOUT_FILE 2>> $STDERR_FILE &
+$BUNDLE exec rake --trace resque:pool >> $STDOUT_FILE 2>> $STDERR_FILE &
 echo $! > $PID_FILE
