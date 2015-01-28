1edc80327d71331d29530cdeb80407ce1178dafe
nicholas a. evans <nick@ekenosen.net>
2015-01-28 17:48:14 -0500

    PooledWorker will not shutdown if we aren't running inside a pool.

--- lib/resque/pool/pooled_worker.rb
+++ lib/resque/pool/pooled_worker.rb
@@ -5,13 +5,17 @@ class Resque::Pool
     attr_accessor :pool_master_pid
     attr_accessor :worker_parent_pid
 
+    # We will return false if there are no potential_parent_pids, because that
+    # means we aren't even running inside resque-pool.
+    #
     # We can't just check if we've been re-parented to PID 1 (init) because we
     # want to support docker (which will make the pool master PID 1).
     #
     # We also check the worker_parent_pid, because resque-multi-jobs-fork calls
     # Worker#shutdown? from inside the worker child process.
     def pool_master_has_gone_away?
-      not potential_parent_pids.include?(Process.ppid)
+      pids = potential_parent_pids
+      pids.any? && !pids.include?(Process.ppid)
     end
 
     def potential_parent_pids
